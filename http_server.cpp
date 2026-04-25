e0c41c1975effddef55202bc71020af9c88f2a3d
mashanov dmitry <mashanovhdhh@gmail.com>
2026-04-25 12:04:06 +0300

    fixed http_server.cpp

--- http_server.cpp
+++ http_server.cpp
@@ -4,27 +4,9 @@
 
 #include <boost/asio.hpp>
 
-// Make sure to link against the Boost.Asio library when compiling this code.
-// how to compile: 
+// working compiltion:
 
-//g++ -std=c++14 hello.cpp -o hello -lboost_system -lpthread
-
-// how to download this library with brew: brew install boost and make sure to link it properly when compiling.
-
-// but whath should i do if i have boost installed but it still says boost/asio.hpp not found?
-// If you have Boost installed but are still getting the error "boost/asio.hpp not found", it may be because the compiler cannot find the Boost headers. 
-// You can specify the include path to the Boost headers using the -I flag when compiling. 
-// For example, if your Boost headers are located in /usr/local/include, you can compile your code like this:
-
-// g++ -std=c++14 hello.cpp -o hello -I/usr/local/include -lboost_system -lpthread
-
-// how can i check what version of c++ i am using?
-// You can check the version of C++ you are using by running the following command in your terminal:
-
-// g++ --version
-
-// This will display the version of the g++ compiler you have installed, which corresponds to the C++ standard it supports. 
-// To check the specific C++ standard version you are using in your project, you can look at the compilation flags in your build system (e.g., Makefile, CMakeLists.txt) to see if you are using -std=c++14 or another version.
+// g++ -std=c++14 http_server.cpp -o http_server -I/opt/homebrew/include -L/opt/homebrew/lib -lpthread && ./http_server
 
 #include <iostream>
 #include <string>
@@ -37,11 +19,11 @@
 // 2. Save the code to a file named hello.cpp.
 // 3. Open a terminal and navigate to the directory where you saved hello.cpp.
 // 4. Compile the code using the following command (adjusting the include path if necessary):
-//    g++ -std=c++14 hello.cpp -o hello -I/usr/local/include -lboost_system -lpthread
+//    g++ -std=c++14 http_server.cpp -o http_server -I/opt/homebrew/include -L/opt/homebrew/lib -lpthread
 // 5. Run the compiled program:
 //    ./hello
 
-// or just g++ -std=c++14 hello.cpp -o hello -lboost_system -lpthread && ./hello
+// or just g++ -std=c++14 http_server.cpp -o http_server -I/opt/homebrew/include -L/opt/homebrew/lib -lpthread && ./http_server
 
 // 6. The server will start and listen on port 8080. You can test it by opening a web browser and navigating to http://localhost:8080, or by using a tool like curl:
 //    curl http://localhost:8080
@@ -52,7 +34,9 @@ int main() {
 		boost::asio::io_context io_context;
 
 		// Create an acceptor to listen for incoming connections on port 8080
+		
 		boost::asio::ip::tcp::acceptor acceptor(io_context, boost::asio::ip::tcp::endpoint(boost::asio::ip::tcp::v4(), 8080));
+		
 		// acceptor - This is an object that listens for incoming TCP connections on a specified endpoint (in this case, port 8080).
 		// boost::asio::ip::tcp::endpoint - This represents an endpoint for TCP communication, which consists of an IP address and a port number. 
 		// Here, we are using boost::asio::ip::tcp::v4() to specify that we want to listen on all available IPv4 addresses.
